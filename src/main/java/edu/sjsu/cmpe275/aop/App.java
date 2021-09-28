abab636d43f2e97bff478a1bb3217604e364b15a
Yi <jinyi06@gmail.com>
2021-09-28 00:48:31 -0700

    initial commit

---
+++ src/main/java/edu/sjsu/cmpe275/aop/App.java
@@ -0,0 +1,33 @@
+package edu.sjsu.cmpe275.aop;
+
+import java.util.UUID;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        /***
+         * Following is a dummy implementation of App to demonstrate bean creation with Application context.
+         * You may make changes to suit your need, but this file is NOT part of your submission.
+         */
+
+    	ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("context.xml");
+        SecretService secretService = (SecretService) ctx.getBean("secretService");
+        SecretStats stats = (SecretStats) ctx.getBean("secretStats");
+
+        try {
+        	UUID secret = secretService.createSecret("Alice", "My little secret");
+        	secretService.shareSecret("Alice", secret, "Bob");
+        	secretService.readSecret("Bob", secret);
+        	secretService.shareSecret("Alice", secret, "Bob");
+        	secretService.readSecret("Bob", secret);      
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("The best known secret: " + stats.getBestKnownSecret());
+        System.out.println("The worst secret keeper: " + stats.getWorstSecretKeeper());
+        System.out.println("The most trusted user: " + stats.getMostTrustedUser());
+        ctx.close();
+    }
+}
---
+++ src/main/java/edu/sjsu/cmpe275/aop/SecretStatsImpl.java
@@ -0,0 +1,44 @@
+package edu.sjsu.cmpe275.aop;
+
+import java.util.UUID;
+
+public class SecretStatsImpl implements SecretStats {
+    /***
+     * This is a dummy implementation only.
+     * You are expected to provide an actual implementation based on the requirements.
+     */
+
+	@Override
+	public void resetStatsAndSystem() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getLengthOfLongestSecret() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getMostTrustedUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getWorstSecretKeeper() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UUID getBestKnownSecret() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+    
+}
+
+
+
